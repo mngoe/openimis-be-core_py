fdd3df3fe5c6e5b6d937b2c67b8d75c5ad971cee
paul sandjong <princesandjong777@gmail.com>
2026-04-23 20:09:28 +0100

    feat(core): enrich /core/users/current_user with claim_admin, officer and extended i_user payload with roles

--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,8 @@
+from django.core.cache import cache
 from rest_framework import serializers
 
 from .apps import CoreConfig
-from .models import User, InteractiveUser, TechnicalUser
-from django.core.cache import cache
+from .models import User, InteractiveUser, TechnicalUser, Role
 from core.utils import get_cache_key
 
 
@@ -20,30 +20,138 @@ class CachedModelSerializer(serializers.ModelSerializer):
         cache.set(cache_key, representation, self.cache_ttl)
         return representation
 
+
+class RoleSummarySerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+
+
+class LocationSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    uuid = serializers.UUIDField()
+    code = serializers.CharField()
+    name = serializers.CharField()
+    type = serializers.CharField()
+    parent = serializers.SerializerMethodField()
+
+    def get_parent(self, obj):
+        if not obj or not getattr(obj, "parent", None):
+            return None
+        return LocationSerializer(obj.parent).data
+
+
+class PricelistSummarySerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    uuid = serializers.CharField()
+
+
+class ProgramSerializer(serializers.Serializer):
+    idProgram = serializers.IntegerField()
+    nameProgram = serializers.CharField()
+
+
+class HealthFacilitySerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    uuid = serializers.UUIDField()
+    code = serializers.CharField()
+    name = serializers.CharField()
+    level = serializers.CharField()
+    servicesPricelist = PricelistSummarySerializer(source="services_pricelist")
+    itemsPricelist = PricelistSummarySerializer(source="items_pricelist")
+    contractStartDate = serializers.DateField(source="contract_start_date")
+    contractEndDate = serializers.DateField(source="contract_end_date")
+    location = LocationSerializer()
+    program = ProgramSerializer(many=True)
+
+
+class OfficerSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    uuid = serializers.UUIDField()
+    code = serializers.CharField()
+    dob = serializers.DateField()
+    address = serializers.CharField()
+    lastName = serializers.CharField(source="last_name")
+    otherNames = serializers.CharField(source="other_names")
+    location = LocationSerializer(read_only=True)
+
+
+class ClaimAdminSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    uuid = serializers.UUIDField()
+    code = serializers.CharField()
+    emailId = serializers.CharField(source="email_id")
+    phone = serializers.CharField()
+    dob = serializers.DateField()
+    lastName = serializers.CharField(source="last_name")
+    otherNames = serializers.CharField(source="other_names")
+    healthFacility = HealthFacilitySerializer(source="health_facility")
+
+
 class InteractiveUserSerializer(serializers.ModelSerializer):
     language = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
     has_password = serializers.SerializerMethodField()
+    roles = serializers.SerializerMethodField()
 
     def get_has_password(self, obj):
         return obj.stored_password != CoreConfig.locked_user_password_hash
 
+    def get_roles(self, obj):
+        user_roles = getattr(obj, "user_roles", None)
+
+        if not user_roles:
+            return []
+
+        user_roles = user_roles.filter(validity_to__isnull=True).select_related("role")
+
+        role_ids = []
+        for user_role in user_roles:
+            if user_role.role_id not in role_ids:
+                role_ids.append(user_role.role_id)
+
+        roles = Role.objects.filter(id__in=role_ids, validity_to__isnull=True)
+        role_map = {r.id: r for r in roles}
+
+        ordered_roles = [role_map[rid] for rid in role_ids if rid in role_map]
+
+        return RoleSummarySerializer(ordered_roles, many=True).data
+
     class Meta:
         model = InteractiveUser
-        fields = ('id', 'language', 'last_name',
-                  'other_names', 'health_facility_id', 'rights', 'has_password')
+        fields = (
+            "id",
+            "language",
+            "last_name",
+            "other_names",
+            "health_facility_id",
+            "rights",
+            "email",
+            "phone",
+            "roles",
+            "has_password",
+        )
 
 
 class TechnicalUserSerializer(serializers.ModelSerializer):
     cache_ttl = 60 * 60
+
     class Meta:
         model = TechnicalUser
-        fields = ('id', 'language', 'username', 'email')
+        fields = ("id", "language", "username", "email")
 
 
 class UserSerializer(serializers.ModelSerializer):
     i_user = InteractiveUserSerializer(many=False, read_only=True)
     t_user = TechnicalUserSerializer(many=False, read_only=True)
+    claim_admin = ClaimAdminSerializer(many=False, read_only=True)
+    officer = OfficerSerializer(many=False, read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'i_user', 't_user')
+        fields = (
+            "id",
+            "username",
+            "i_user",
+            "t_user",
+            "claim_admin",
+            "officer",
+        )
