75b9b001b5446fa7b544024f881d85efa8d243c7
kevel choumene <k.choumene@cloudfacile.com>
2026-06-30 14:57:23 +0100

    fix(SubmitClaims): fix submit claim when user submits all claims at once ref36952

--- core/utils.py
+++ core/utils.py
@@ -17,6 +17,7 @@ from django.apps import apps
 from django.core.exceptions import PermissionDenied, FieldDoesNotExist
 from django.core.files.storage import default_storage
 from django.core.cache import caches
+import threading
 
 logger = logging.getLogger(__file__)
 
@@ -39,6 +40,17 @@ __all__ = [
     "ExtendedRelayConnection",
 ]
 
+_request_local = threading.local()
+
+def get_current_user():
+    return getattr(_request_local, "user", None)
+
+def set_current_user(user):
+    _request_local.user = user
+
+def clear_current_user():
+    if hasattr(_request_local, "user"):
+        del _request_local.user
 
 class TimeUtils(object):
 
